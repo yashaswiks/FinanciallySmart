5c5f9726333474336f38c0f51be3b7c77c2d7249
yashaswiks <63037516+yashaswiks@users.noreply.github.com>
2022-05-14 23:54:41 +0530

    SQL Server Configuration implemented.
    App.Config created.
    SQLServerModel Class created.

---
+++ Models/SQLServerModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinanciallySmart.Models
+{
+    internal class SQLServerModel
+    {
+        private string connectionString = ConfigurationManager.ConnectionStrings["sQLServerConnectionString"].ConnectionString;
+
+        public string GetConnectionString()
+        {
+            return connectionString;
+        }
+    }
+}
